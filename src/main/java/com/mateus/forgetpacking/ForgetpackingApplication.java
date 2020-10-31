b300dcbf555badfd1c911e6ee0d9018ca7bb02f1
Mateus Dutra Dadalto <mateusdadalto@gmail.com>
2020-10-31 13:22:28 -0300

    First Commit

---
+++ src/main/java/com/mateus/forgetpacking/ForgetpackingApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.forgetpacking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgetpackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForgetpackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateus/forgetpacking/ForgetpackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.forgetpacking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgetpackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
